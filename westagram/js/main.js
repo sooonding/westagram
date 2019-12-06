4ac6c21f382a554275b9f1fa9f6031fe80abca7a
전성진 <sooonding@gmail.com>
2019-12-06 11:59:33 +0900

    댓글삭제 기능 추가

--- westagram/js/main.js
+++ westagram/js/main.js
@@ -1,26 +1,38 @@
 function index() {
   const input = document.querySelector(".comment-input");
-
+  // 제출 버튼
   const submit = document.querySelector(".comment-make");
 
   //붙이려는 함수 사용
   function make() {
-    const ul = document.querySelector(".comment-ul");
+    const ul = document.querySelector(".comment-ul")
     const li = document.createElement("li");
-    li.className = "comment";
-
     const inputValue = input.value;
     const p = document.createElement("p");
     const span = document.createElement("span");
+    //삭제 버튼
+    const delBtn = document.createElement('button');
+
+    delBtn.className = 'del';
+    li.className = "comment";
     p.className = "your-name";
     span.className = "your-comment";
-    p.innerHTML = "wecode";
+    p.innerHTML = "hong";
     span.innerHTML = inputValue;
+    delBtn.innerHTML = '❌';
+    delBtn.style.border = 'none';
+
     ul.appendChild(li);
     li.appendChild(p);
     li.appendChild(span);
+    li.appendChild(delBtn);
+
     input.value = ''; // check;
     console.log(1);
+
+    delBtn.addEventListener('click',function(){
+      ul.removeChild(li);
+    })
   }
   //event
   input.addEventListener("keyup",enter);
@@ -34,6 +46,5 @@ function index() {
   function click() {
       make();
   }
-
 }
 index();
